6ba4f89d7880637775cb8316ef465f6486a01c69
ManDo1204 <84727553+ManDo1204@users.noreply.github.com>
2024-04-20 10:38:28 +0700

    create api

--- backend/vtv_crawl/international/urls.py
+++ backend/vtv_crawl/international/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from international.services.test import Test
-
+from international.services.test.test import Test
+from international.services.get_list.view import GetListNew
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from international.services.register.view import UserRegisterView
 
 urlpatterns = [
-    path('test/', Test.as_view())
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', UserRegisterView.as_view(), name='user_register'),
+    path('test/', Test.as_view()),
+    path('get-list/', GetListNew.as_view()),
 ]
--- backend/vtv_crawl/vtv_crawl/settings.py
+++ backend/vtv_crawl/vtv_crawl/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -32,13 +32,27 @@ ALLOWED_HOSTS = ["manhdo.me"]
 
 INSTALLED_APPS = [
     'international',
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -51,6 +65,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'vtv_crawl.urls'
+
+AUTH_USER_MODEL = 'international.User'
 
 TEMPLATES = [
     {
